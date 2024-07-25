afafbc54a24f70e041e03464d58f9703656c85de
Madhusha Laksitha <67388969+MadhushaSUM@users.noreply.github.com>
2024-07-25 23:15:42 +0530

    Bundling functions with Webpack

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ must have terraform.tfvars file with these data
 access_key = "YOUR_AWS_ACCESS_KEY"
 
 secret_key = "YOUR_AWS_SECRET_KEY"
+
+Use following command to bundle code with Webpack
+npx webpack --config webpack.config.js
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    'patient/create-patient': './src/functions/patient/create-patient/index.ts',
+    // 'patient/get-patient-by-id': './src/functions/patient/get-patient-by-id/index.ts',
+    // 'doctor/create-doctor': './src/functions/doctor/create-doctor/index.ts',
+    // 'doctor/get-doctor-by-id': './src/functions/doctor/get-doctor-by-id/index.ts',
+    // 'investigation/get-all-investigations': './src/functions/investigation/get-all-investigations/index.ts',
+    // 'investigation/get-investigation-by-id': './src/functions/investigation/get-investigation-by-id/index.ts',
+  },
+  target: 'node',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name]/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+  },
+};
